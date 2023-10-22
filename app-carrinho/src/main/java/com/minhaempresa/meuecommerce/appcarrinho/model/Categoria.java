9161fe0a753059dd26226bc550b8c8b40c7b80be
Pedro Oliveira <pedro_hlto@hotmail.com>
2023-10-22 13:34:39 -0300

    primeiro commit carrinho

---
+++ app-carrinho/src/main/java/com/minhaempresa/meuecommerce/appcarrinho/model/Categoria.java
@@ -0,0 +1,23 @@
+package com.minhaempresa.meuecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+
+}
